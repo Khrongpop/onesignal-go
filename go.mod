02b1bcca6d258d21055cf37ab263aa405e144af3
Khrongpop <mr.khrongpop@gmail.com>
2019-04-02 15:13:11 +0700

    push function

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/khrongpop/onesignal-go
+
+go 1.12
+
+require github.com/tbalthazar/onesignal-go v0.0.0-20160928064723-312530be66c8
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
@@ -16,31 +17,9 @@ var client = onesignal.NewClient(nil)
 func main() {
 	client.UserKey = appID
 	client.AppKey = appKey
-	// listOpt := &onesignal.NotificationListOptions{
-	// 	AppID:  appID,
-	// 	Limit:  10,
-	// 	Offset: 0,
-	// }
-	// _, _, _ := client.Notifications.List(listOpt)
-
-	// playerID := "db480b54-6734-4957-9be8-2a7b8d2b0f20"
-	// notificationReq := &onesignal.NotificationRequest{
-	// 	AppID:    appID,
-	// 	Contents: map[string]string{"en": "English message"},
-	// 	IsIOS:    true,
-	// 	IsChrome: true,
-	// 	// IncludePlayerIDs: []string{playerID},
-	// 	IncludedSegments: []string{"All"},
-	// }
-	// _, res, err := client.Notifications.Create(notificationReq)
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// fmt.Println(res)
-	// body, _ := ioutil.ReadAll(res.Body)
-	// fmt.Println("response Body:", string(body))
 
 	http.HandleFunc("/", index)
+	http.HandleFunc("/push", push)
 	port := os.Getenv("PORT")
 	fmt.Println(`start port http:localhost:` + port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
@@ -48,17 +27,30 @@ func main() {
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-	client := onesignal.NewClient(nil)
-	client.UserKey = appID
-	client.AppKey = appKey
-	// appRequest := &onesignal.AppRequest{
-	// 	Name: "Your app 1",
-	// }
-	// app, res, err := client.Apps.Create(appRequest)
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// fmt.Println(res)
-	// fmt.Println(app)
 
+}
+
+func push(w http.ResponseWriter, r *http.Request) {
+	// playerID := "db480b54-6734-4957-9be8-2a7b8d2b0f20"
+	notificationReq := &onesignal.NotificationRequest{
+		AppID:    appID,
+		Contents: map[string]string{"en": "English message"},
+		IsIOS:    true,
+		IsChrome: true,
+		// IncludePlayerIDs: []string{playerID},
+		IncludedSegments: []string{"All"},
+	}
+	NotificationCreateResponse, res, err := client.Notifications.Create(notificationReq)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(res)
+	fmt.Print(`NotificationCreateResponse : `)
+	fmt.Println(NotificationCreateResponse)
+	fmt.Print(`*NotificationCreateResponse : `)
+	fmt.Println(*NotificationCreateResponse)
+	fmt.Print(`&NotificationCreateResponse : `)
+	fmt.Println(&NotificationCreateResponse)
+	body, _ := ioutil.ReadAll(res.Body)
+	fmt.Println("response Body:", string(body))
 }
